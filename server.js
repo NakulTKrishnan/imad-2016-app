e885b0719eef33f3444fae9c30fc96437cc6f0e2
NakulTKrishnan <nakultk@gmail.com>
2016-11-27 01:40:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,14 +13,13 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/style.scss', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.scss'));
+app.get('/ui/bg1.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'bg1.jpg'));
 });
 
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+app.get('/ui/bg2.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'bg2.jpg'));
 });
-
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
